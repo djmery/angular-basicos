aeba32ada1ce52fe00cb9f47b296c58d6ed4ecbc
silvia <djmery76@gmail.com>
2022-04-03 20:19:56 +0200

    Cambios Modulo dbz

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+
 })
 export class MainPageComponent implements OnInit {
+
+  personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 15000 },
+    { nombre: 'Vegeta', poder: 8500 }
+  ]
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Rogui',
+    poder: 1000
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  /*agregar(event: any) {
+    event.preventDefault();
+    console.log('Heyy...');
+  } */
+
+  agregarNuevoPersonaje(Personaje: Personaje) {
+    this.personajes.push(Personaje);
+  }
+
 
 }
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+
+})
+export class PersonajesComponent implements OnInit {
+
+  @Input() personajes: Personaje[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
